953b89f0ae3a6abd9fcd1bb7fce491f19b7e4102
Jessicamf4 <jessicamarquesfialho0@gmail.com>
2023-12-04 19:28:53 -0300

    Algoritmo Guloso

---
+++ AlgoritmoGuloso/CaixeiroViajante/MelhorRota.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaixeiroViajante
+{
+    internal class MelhorRota
+    {
+        public double[,] DistanceMatrix { get; set; }
+
+        public MelhorRota(double[,] distanceMatrix)
+        {
+            this.DistanceMatrix = distanceMatrix;
+        }
+
+    public void CalculaMelhorRota()
+        {
+
+            int cidades = DistanceMatrix.GetLength(0);
+            int[] rota = new int[cidades];
+            bool[] visitado = new bool[cidades];
+
+            rota[0] = 0;
+            visitado[0] = true;
+
+            for(int i = 1; i < cidades; i++) 
+            {
+                int cidadeAtual = rota[i-1];
+                int proximaCidade = -1;
+                double menorDistancia = double.PositiveInfinity;
+                for(int j = 0; j < cidades; j++)
+                {
+                    if (!visitado[j] && DistanceMatrix[cidadeAtual, j] < menorDistancia && DistanceMatrix[cidadeAtual, j] != DistanceMatrix[cidadeAtual,cidadeAtual])
+                    {
+                        menorDistancia = DistanceMatrix[cidadeAtual, j];
+                        proximaCidade = j;
+                    }
+                }
+
+                if (proximaCidade != -1)
+                {
+                    rota[i] = proximaCidade;
+                    visitado[proximaCidade] = true;
+                }
+            }
+
+            //rota[cidades - 1] = rota[0]; 
+
+            Console.WriteLine("Melhor Rota Encontrada:");
+            for (int i = 0; i < cidades; i++)
+            {
+                Console.Write(rota[i] + " -> ");
+            }
+            Console.WriteLine(rota[0]); // Volta para a primeira cidade para fechar o ciclo
+
+
+        }
+
+    }
+    
+}
+
+
+
+
+
---
+++ AlgoritmoGuloso/CaixeiroViajante/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace CaixeiroViajante
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double[,] distanceMatrix = {
+            {0, 10, 15, 20},
+            {10, 0, 35, 25},
+            {15, 35, 0, 30},
+            {20, 25, 30, 0}
+        };
+
+            Console.WriteLine("Matriz de distâncias entre as cidades:");
+            for (int i = 0; i < distanceMatrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < distanceMatrix.GetLength(1); j++)
+                {
+                    Console.Write(distanceMatrix[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+            MelhorRota melhorRota = new MelhorRota(distanceMatrix);
+            melhorRota.CalculaMelhorRota();
+
+        }
+    }
+
+}
